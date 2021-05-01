38c59e40bb4dd71ed2625b60012abeab0902dd0c
Yusuf <ybasori@gmail.com>
2021-05-02 04:23:51 +0700

    update

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2020.3.4f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\Yusuf\Desktop\Projects\unity\NetworkingTutorial" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
